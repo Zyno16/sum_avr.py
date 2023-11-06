4c7734af011373369f431babf5953c3a21c107a5
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-06 11:12:16 +0000

    Add files via upload

---
+++ sum_avr.py
@@ -0,0 +1,14 @@
+
+#this to calculate the sum and one of the way to add the number one after one
+count =0
+s=0
+for i in [1,7,37,45,54,67,74]:
+   
+      count = count + i
+      s=s+1
+      avr= count/s
+print ("the sum is = ",count)
+
+print ("the averge =",avr)
+
+
